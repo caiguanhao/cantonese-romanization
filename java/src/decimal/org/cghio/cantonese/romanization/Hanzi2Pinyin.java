03f5630a471b527bf95fda348e225f2a0ea2f5d0
caiguanhao <caiguanhao@gmail.com>
2013-09-30 18:32:06 +0800

    add decimal java files;

---
+++ java/src/decimal/org/cghio/cantonese/romanization/Hanzi2Pinyin.java
@@ -0,0 +1,25 @@
+package org.cghio.cantonese.romanization;
+
+public class Hanzi2Pinyin {
+
+  /**
+   * Returns Cantonese Pinyin of a Chinese character.
+   * @param character the Chinese character to convert
+   */
+  public static String fromChar(String character) throws java.io.UnsupportedEncodingException {
+    if (character == null || character.isEmpty()) return null;
+
+    return fromChar(Character.codePointAt(character, 0));
+  }
+
+  /**
+   * Returns Cantonese Pinyin of a Chinese character.
+   * @param decimal value of Chinese character
+   */
+  public static String fromChar(int decimal) {
+    String pinyin = Hanzi2PinyinData1.fromChar(decimal);
+    if (pinyin == null) pinyin = Hanzi2PinyinData2.fromChar(decimal);
+    return pinyin;
+  }
+
+}
